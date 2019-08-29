31dd674100ab1899fa7cae6b39d3d9aa29c2f13c
Adenilson Castro <adenilsoncastro@hotmail.com.br>
2019-08-29 08:29:35 -0300

    chamada das features, criação da rede neural on hold

--- main.m
+++ main.m
@@ -2,3 +2,16 @@
 % Universidade Federal do Paraná
 % Programa de Pós-Graduação em Engenharia Elétrica
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%generate signals with following modulations: QAM4, QAM16, PSK2
+snr = [-5 0 5];                                     %SNR vector
+n_frames = 5;                                       %number of frames
+
+signal_qam4 = features(snr, n_frames, 'QAM4');
+signal_qam16 = features(snr, n_frames, 'QAM16');
+signal_psk2 = features(snr, n_frames, 'PSK2');
+
+%%Create and train a RNA
+n_targes=3; %By now, only three modulations are under test
+len_vec=length(signal_qam4(:,:,1))
+
