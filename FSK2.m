633d619ee8ed27a15fe92fc8f6133bb4bcc40a80
ronnymilleo <ronnysrmilleo@gmail.com>
2019-10-10 21:04:26 -0300

    Tradução para o inglês; Alteração de parâmetros chave (frameSize, freqSep, Fs) para adequar o sinal e corrigir erro na geração; Correção de erro massiva para FSK4, onde estavam sendo gerados 4*frameSize bits sem necessidade e a função pedia entrada em símbolos, então o randi deve ir de 0 a M-1.

--- FSK2.m
+++ FSK2.m
@@ -1,29 +1,27 @@
 function [dataOut] = FSK2(frameSize,numSamplesPerSymbol,SNR,phaseFlag,noiseFlag)
-%% Geração do sinal FSK-2
-M = 2;                                                      % Tamanho da constelação
-k = log2(M);                                                % Número de bits por símbolo
-n = k*frameSize;                                            % Número de bits a processar
-freqSep = 10e3;                                             % Separação entre as frequẽncias da modulação
-Fs = 2*freqSep;                                             % Frequência de amostragem da simulação
+%% Signal generation
+M = 2; % Number of symbols
+n = frameSize; % Frame size in bits
+freqSep = numSamplesPerSymbol; % Frequency separation
+Fs = M*numSamplesPerSymbol; % Sampling frequency
 
-% Configuração da mensagem
-dataIn = randi([0 1],n,1);  % Cria o vetor da mensagem
+% Message
+dataIn = randi([0 1],n,1); % Create random message
 
-% Aplicação da modulação
-dataMod = fskmod(dataIn,M,freqSep,numSamplesPerSymbol, Fs);    % Gray coding, phase offset = 0
-initPhase = pi*(2*rand(1)-1);
+% Modulation
+dataMod = fskmod(dataIn,M,freqSep,numSamplesPerSymbol, Fs);
+initPhase = pi*(2*rand(1)-1); % Generate random phase
 
 if(phaseFlag == 1)
-    dataMod = dataMod.*exp(1i*initPhase);                       % Aplicação da fase inicial
+    dataMod = dataMod.*exp(1i*initPhase); % Add random phase
 end
 
 txSignal = dataMod;
 
 if(noiseFlag == 1)
     if(SNR < 50)
-        % SNR e canal
         snr = SNR;
-        rxSignal = awgn(txSignal, snr, 'measured');                 % Aplicação do ruído do canal
+        rxSignal = awgn(txSignal, snr, 'measured'); % Channel noise
     else
         rxSignal = txSignal;
     end
@@ -31,13 +29,6 @@ else
     rxSignal = txSignal;
 end
 
-dataOut = rxSignal;
-
-% Demodulação
-%dataOut = fskdemod(rxSignal,M,freqSep,numSamplesPerSymbol,Fs);
-
-% Cálculo da taxa de erro
-%[numErrors, ber] = biterr(dataIn, dataOut);
-%fprintf('\nBER = %5.2e, baseado em %d erros\n', ...
-%    ber, numErrors)
+dataOut = rxSignal/rms(rxSignal);
+dataOut = dataOut(1:end-numSamplesPerSymbol);
 end
